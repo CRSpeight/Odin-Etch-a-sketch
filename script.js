da2647a272adb8f00d47afc82a006b1bcf804c89
Chris Speight <paradisio15@gmail.com>
2020-07-18 16:22:11 -0500

    Base requirements complete, need to add error handing for input

--- script.js
+++ script.js
@@ -1,19 +1,36 @@
-const initGridSize = 16;
 const grid = document.querySelector("#grid");
 const cell = document.createElement("div");
+const clrBtn = document.querySelector("#clearBtn");
 
-grid.style.gridTemplateColumns = `repeat(${initGridSize}, 1fr)`;
+function gridBuilder(gridSize = 16) {
+  grid.innerHTML = "";
+  grid.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
 
-for (let i = 1; i <= initGridSize * initGridSize; i++) {
-  const cell = document.createElement("div");
-  cell.classList.add("cell");
-  cell.id = i;
-  grid.appendChild(cell);
+  for (let i = 1; i <= gridSize * gridSize; i++) {
+    const cell = document.createElement("div");
+    cell.classList.add("cell");
+    cell.id = i;
+    grid.appendChild(cell);
+  }
+
+  const gridCells = document.querySelectorAll(".cell");
+  gridCells.forEach((c) => {
+    c.addEventListener("mouseover", (e) => {
+      e.target.classList.add("coloredCell");
+    });
+  });
 }
 
-const gridCells = document.querySelectorAll(".cell");
-gridCells.forEach((c) => {
-  c.addEventListener("mouseover", (e) => {
-    e.target.classList.add("coloredCell");
-  });
+function clearGrid() {
+  let desiredCells = prompt(
+    "How many squares wide and tall do you want the grid?",
+    "16"
+  );
+  gridBuilder(desiredCells);
+}
+
+clrBtn.addEventListener("click", () => {
+  clearGrid();
 });
+
+gridBuilder();
